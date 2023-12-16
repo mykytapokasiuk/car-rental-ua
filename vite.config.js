4b214649b79ad03936c622c4d71bd15f810f6172
Mykyta Pokasiuk <mykyta.pokasiuk@ukr.net>
2023-12-16 23:08:59 +0200

    Bugs fixed

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,40 @@
-import { defineConfig, loadEnv } from 'vite';
+// import { defineConfig, loadEnv } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+// import svgr from 'vite-plugin-svgr';
+
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), '');
+
+//   return {
+//     define: {
+//       'process.env.BASE_URL': JSON.stringify(env.BASE_URL),
+//     },
+//     plugins: [react(), svgr()],
+//     resolve: {
+//       alias: {
+//         src: '/src',
+//         components: '/src/components',
+//       },
+//     },
+//     base: '/easy-drive-ua',
+//   };
+// });
+
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
+import 'dotenv/config';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    define: {
-      'process.env.BASE_URL': JSON.stringify(env.BASE_URL),
+export default defineConfig({
+  define: {
+    'process.env': process.env,
+  },
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
     },
-    plugins: [react(), svgr()],
-    resolve: {
-      alias: {
-        src: '/src',
-        components: '/src/components',
-      },
-    },
-    base: '/easy-drive-ua',
-  };
+  },
+  base: '/easy-drive-ua',
 });
