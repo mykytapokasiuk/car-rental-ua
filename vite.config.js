c5f84301e42ea43be953af00528c6285729091d5
Mykyta Pokasiuk <mykyta.pokasiuk@ukr.net>
2023-12-17 00:28:19 +0200

    Bugs fixed

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,3 @@
-// import { defineConfig, loadEnv } from 'vite';
-// import react from '@vitejs/plugin-react-swc';
-// import svgr from 'vite-plugin-svgr';
-
-// export default defineConfig(({ mode }) => {
-//   const env = loadEnv(mode, process.cwd(), '');
-
-//   return {
-//     define: {
-//       'process.env.BASE_URL': JSON.stringify(env.BASE_URL),
-//     },
-//     plugins: [react(), svgr()],
-//     resolve: {
-//       alias: {
-//         src: '/src',
-//         components: '/src/components',
-//       },
-//     },
-//     base: '/easy-drive-ua',
-//   };
-// });
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
